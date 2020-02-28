8d7a80010b80f193dbe5bb711e9e7b09352f5c05
Abhay Pathare <61594986+abhayspathare@users.noreply.github.com>
2020-02-28 22:19:41 +0530

    Update HelloWorld.py
    OOP Code for greetings

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
-print ("Hello World")
+class Greetings:
+    def __init__(self):
+        self.welcomeMsg = "Hello world"
+
+    def getWelcomeMsg(self):
+        return self.welcomeMsg
+
+g1 = Greetings ()
+print g1.getWelcomeMsg()
